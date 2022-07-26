b054609a8c8938208919b6a25694bdfd75dbaab5
thales-pinner <thales.neves@pinpeople.com.br>
2022-07-26 00:13:25 -0300

    basic db funcs

---
+++ pkg/config/app.go
@@ -0,0 +1,25 @@
+package config
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	database, err := gorm.Open("mysql", "thneves:simplepass/simplerest?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+
+	db = database
+}
+
+func getDB() *gorm.DB {
+	return db
+}
+
+// the whole poiunt is to return a db file
