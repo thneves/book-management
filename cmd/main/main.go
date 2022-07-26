dd872562b5dc05964401b8d02fd0157802c78264
thales-pinner <thales.neves@pinpeople.com.br>
2022-07-26 00:35:56 -0300

    add server listeners to main file

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialetcs/mysql"
+	"github.com/thneves/go-bookstore/pkg/routes"
+	"log"
+	"net/http"
+)
 
+func main() {
+	route := mux.NewRouter()
+	routes.RegisterBooksStoreRoutes(route)
+	http.Handle("/", route)
+	log.Fatal(http.ListenAndServe("localhost:8080", route))
 }
