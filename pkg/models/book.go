54103ba382c8e4e1868dfb6cbef496128f9b13e3
thales-pinner <thales.neves@pinpeople.com.br>
2022-07-28 23:45:37 -0300

    Add book model functions

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -19,3 +19,30 @@ func init() {
 	db = config.getDB()
 	db.AutoMigrate(&Book{})
 }
+
+func (b *Book) CreateBook() *Book {
+	db.NewRecord(b)
+	db.Create(&b)
+	return b
+}
+
+func GetAllBooks() []Book {
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+func GetBookById(Id int64) (*Book, *gorm.DB) {
+	var getBook Book
+	db := db.Where("ID=?", Id).Find(&getBook)
+	return &getBook, db
+}
+
+func DeleteBook(ID int64) Book {
+	var book Book
+	db.Where("ID=?", ID).Delete(Book)
+	return book
+}
+
+// No Update Function, cause we delete the book and create a new one
+// Get, Delete, and Delete
